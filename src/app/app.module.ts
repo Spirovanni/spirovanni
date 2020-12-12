7eda4c4583b12421046a050cb6829ca26b021326
Xavier Martinez <blackshieldsx@gmail.com>
2020-12-12 08:51:54 -0800

    Innitializing the router in the app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AuthModule } from './@auth/auth/auth.module';
 import { CoreModule } from './@core/core/core.module';
 
 import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'actions'}
+];
 
 @NgModule({
   declarations: [
