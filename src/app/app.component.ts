8e9b73d2fd3297d17af5da2652d67c7baee09286
Xavier Martinez <blackshieldsx@gmail.com>
2020-12-12 17:50:19 -0800

    Add additional modules
    PerfectScrollbarModule,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {
+  trigger, state, style, animate, transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger, state, style, animate, transition,
+    // animation triggers go here
+  ]
 })
 export class AppComponent {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { CustomOverlayContainer } from './@theme/utils/custom-overlay-container';
+
+// import { AgmCoreModule } from '@agm/core';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthModule } from './@auth/auth/auth.module';
@@ -12,21 +20,31 @@ const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'actions'}
 ];
 
+// noinspection AngularInvalidImportedOrDeclaredSymbol
 @NgModule({
-  declarations: [
-    AppComponent,
-    ActionListComponent
-  ],
   imports: [
     BrowserModule,
+    // BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    // AgmCoreModule.forRoot({
+    //   apiKey: 'AIzaSyAO7Mg2Cs1qzo_3jkKkZAKY6jtwIlm41-I'
+    // }),
+    PerfectScrollbarModule,
     AuthModule,
     CoreModule,
     RouterModule.forRoot(routes)
   ],
+  declarations: [
+    AppComponent,
+    ActionListComponent
+  ],
   exports: [
     RouterModule
   ],
-  providers: [],
+  providers: [
+    { provide: OverlayContainer, useClass: CustomOverlayContainer }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
