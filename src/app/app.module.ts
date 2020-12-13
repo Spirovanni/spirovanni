265a22ca5754fb5e5178ee24e74580557bd93444
Xavier Martinez <blackshieldsx@gmail.com>
2020-12-12 18:36:17 -0800

    Adding calendar module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,22 @@ import { CustomOverlayContainer } from './@theme/utils/custom-overlay-container'
 
 // import { AgmCoreModule } from '@agm/core';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  wheelPropagation: true,
+  suppressScrollX: true
+};
+
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthModule } from './@auth/auth/auth.module';
 import { CoreModule } from './@core/core/core.module';
 
+import { AppSettings } from './app.settings';
 import { AppComponent } from './app.component';
 import { ActionListComponent } from './@components/action-list/action-list.component';
 
@@ -31,6 +41,10 @@ const routes: Routes = [
     //   apiKey: 'AIzaSyAO7Mg2Cs1qzo_3jkKkZAKY6jtwIlm41-I'
     // }),
     PerfectScrollbarModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    }),
     AuthModule,
     CoreModule,
     RouterModule.forRoot(routes)
@@ -43,6 +57,8 @@ const routes: Routes = [
     RouterModule
   ],
   providers: [
+    AppSettings,
+    { provide: PERFECT_SCROLLBAR_CONFIG, useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG },
     { provide: OverlayContainer, useClass: CustomOverlayContainer }
   ],
   bootstrap: [AppComponent]
