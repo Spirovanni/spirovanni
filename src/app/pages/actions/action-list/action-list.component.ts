2f94fa4ce19210526cf808909570e7efbd89c64e
Xavier Martinez <blackshieldsx@gmail.com>
2020-12-13 16:30:48 -0800

    Changes to user info

--- src/app/pages/actions/action-list/action-list.component.ts
+++ src/app/pages/actions/action-list/action-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { AppSettings } from '../../../app.settings';
+import { Settings } from '../../../app.settings.model';
+import { TablesService, Element } from '../../tables/tables.service';
 
 @Component({
   selector: 'app-action-list',
   templateUrl: './action-list.component.html',
   styleUrls: ['./action-list.component.scss']
 })
-export class ActionListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ActionListComponent {
+  public displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  public dataSource: any;
+  public settings: Settings;
+  constructor(public appSettings: AppSettings, private tablesService: TablesService) {
+    this.settings = this.appSettings.settings;
+    this.dataSource = new MatTableDataSource<Element>(this.tablesService.getData());
   }
-
 }
--- src/app/pages/actions/actions.module.ts
+++ src/app/pages/actions/actions.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { TablesService } from './../tables/tables.service';
 import { SharedModule } from '../../@core/shared/shared.module';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
@@ -25,10 +27,14 @@ export const routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    NgxDatatableModule,
     SharedModule,
     FormsModule,
     NgxChartsModule,
     PerfectScrollbarModule
+  ],
+  providers: [
+    TablesService
   ]
 })
 export class ActionsModule { }
