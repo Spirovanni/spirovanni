c3bcfc8eb60d5b896ddfc4fed7866a7f7f4a288c
Xavier Martinez <blackshieldsx@gmail.com>
2020-12-19 20:08:58 -0800

    Created dynamicCards
      getCards() {
        const dynamicCards = this.httpClient.get(this.baseUrl);
        console.log(dynamicCards);
        return this.cards;
      }

--- src/app/@core/services/api.service.ts
+++ src/app/@core/services/api.service.ts
@@ -8,9 +8,13 @@ export class ApiService {
   baseUrl = 'http://127.0.0.1:8000/api/cards/';
   private cards = ['Mercy Card', 'Happy Card'];
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
   // tslint:disable-next-line:typedef
   getCards() {
+    const dynamicCards = this.httpClient.get(this.baseUrl);
+    console.log(dynamicCards);
     return this.cards;
   }
 }
