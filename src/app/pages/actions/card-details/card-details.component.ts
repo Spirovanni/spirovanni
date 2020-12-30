ec9dbe809737b3d4a5b68ab7c83ba9a7e0b3491b
Xavier Martinez <blackshieldsx@gmail.com>
2020-12-29 19:06:56 -0800

    Corrected the component

--- src/app/pages/actions/card-details/card-details.component.ts
+++ src/app/pages/actions/card-details/card-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-action-details',
+  selector: 'app-card-details',
   templateUrl: './card-details.component.html',
   styleUrls: ['./card-details.component.scss']
 })
