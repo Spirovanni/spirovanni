ae95f32fd0f2ff5635482a579992a892bbb2a058
Xavier Martinez <blackshieldsx@gmail.com>
2021-01-17 13:17:24 -0800

    created loginUser
      loginUser(authData) {
        const body = JSON.stringify({authData});
        return this.httpClient.put(`${this.baseUrl}auth/`, body, {headers: this.headers});
      }

--- src/app/@core/services/api.service.ts
+++ src/app/@core/services/api.service.ts
@@ -6,7 +6,8 @@ import {Card} from '../models/Card';
   providedIn: 'root'
 })
 export class ApiService {
-  baseUrl = 'http://127.0.0.1:8000/api/cards/';
+  baseUrl = 'http://127.0.0.1:8000/';
+  baseCardUrl = `${this.baseUrl}api/cards/`;
   headers = new HttpHeaders({
     'Content-Type': 'application/json',
     Authorization: 'Token 069bbc38f30919f10c0635ac26d795db437bd4ad'
@@ -15,27 +16,28 @@ export class ApiService {
     private httpClient: HttpClient
   ) { }
   getCards() {
-    return this.httpClient.get<Card[]>(this.baseUrl, {headers: this.headers});
+    return this.httpClient.get<Card[]>(this.baseCardUrl, {headers: this.headers});
   }
   getCard(id: number) {
-    return this.httpClient.get<Card>(`${this.baseUrl}${id}/`, {headers: this.headers});
+    return this.httpClient.get<Card>(`${this.baseCardUrl}${id}/`, {headers: this.headers});
   }
   createCard(title: string, description: string) {
     const body = JSON.stringify({title, description});
-    return this.httpClient.post(`${this.baseUrl}`, body, {headers: this.headers});
+    return this.httpClient.post(`${this.baseCardUrl}`, body, {headers: this.headers});
   }
   updateCard(id: number, title: string, description: string) {
     const body = JSON.stringify({title, description});
-    return this.httpClient.put(`${this.baseUrl}${id}/`, body, {headers: this.headers});
+    return this.httpClient.put(`${this.baseCardUrl}${id}/`, body, {headers: this.headers});
   }
   deleteCard(id: number) {
-    return this.httpClient.delete(`${this.baseUrl}${id}/`, {headers: this.headers});
+    return this.httpClient.delete(`${this.baseCardUrl}${id}/`, {headers: this.headers});
   }
   rateCard(rate: number, cardId: number) {
     const body = JSON.stringify({stars: rate});
-    return this.httpClient.post(`${this.baseUrl}${cardId}/rate_card/`, body, {headers: this.headers});
+    return this.httpClient.post(`${this.baseCardUrl}${cardId}/rate_card/`, body, {headers: this.headers});
   }
   loginUser(authData) {
-
+    const body = JSON.stringify({authData});
+    return this.httpClient.put(`${this.baseUrl}auth/`, body, {headers: this.headers});
   }
 }
